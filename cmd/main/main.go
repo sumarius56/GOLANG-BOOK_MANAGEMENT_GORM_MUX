a8141f4a5b57332ab435a21c5191d8405b9c7596
Suciu Marius <110302280+sumarius56@users.noreply.github.com>
2022-11-30 22:41:27 +0200

    it works

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"go-bookstore/pkg/routes"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"go-bookstore/pkg/routes"
-
 )
 
 func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":9010", r))
 }
+
+// https://youtu.be/jFfo23yIWac?t=8337
